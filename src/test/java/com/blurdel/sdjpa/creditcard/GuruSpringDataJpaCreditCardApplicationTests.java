76ddb094db1ba4f9c4a82657d8ec957352b43d49
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-30 16:58:28 -0400

    Added JdbcTemplate to read table w/o hibernate

--- src/test/java/com/blurdel/sdjpa/creditcard/GuruSpringDataJpaCreditCardApplicationTests.java
+++ src/test/java/com/blurdel/sdjpa/creditcard/GuruSpringDataJpaCreditCardApplicationTests.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,6 +27,9 @@ class GuruSpringDataJpaCreditCardApplicationTests {
 	@Autowired
 	CreditCardRepository creditCardRepo;
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
 
 	@Test
 	void testSaveAndStoreCreditCard() {
@@ -38,6 +44,14 @@ class GuruSpringDataJpaCreditCardApplicationTests {
 
 		System.out.println("CC at Rest");
 		System.out.println("CC encrypted: " + encryptionService.encrypt(CREDIT_CARD));
+
+		Map<String, Object> dbRow = jdbcTemplate.queryForMap("select * from credit_card where id = " + saved.getId());
+
+		String dbCardValue = (String) dbRow.get("credit_card_number");
+
+		assertThat(saved.getCreditCardNumber()).isNotEqualTo(dbCardValue);
+		assertThat(dbCardValue).isEqualTo(encryptionService.encrypt(CREDIT_CARD));
+
 
 		CreditCard fetched = creditCardRepo.findById(saved.getId()).get();
 
