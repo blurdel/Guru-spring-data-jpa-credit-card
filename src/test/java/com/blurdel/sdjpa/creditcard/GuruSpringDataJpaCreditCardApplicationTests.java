640263beb2a98ca0a69ca31304c0e710ce5cc1da
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-30 16:21:59 -0400

    Added initial unit test to store credit card

--- src/test/java/com/blurdel/sdjpa/creditcard/GuruSpringDataJpaCreditCardApplicationTests.java
+++ src/test/java/com/blurdel/sdjpa/creditcard/GuruSpringDataJpaCreditCardApplicationTests.java
@@ -1,13 +1,40 @@
 package com.blurdel.sdjpa.creditcard;
 
+import com.blurdel.sdjpa.creditcard.domain.CreditCard;
+import com.blurdel.sdjpa.creditcard.repositories.CreditCardRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("mysql")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class GuruSpringDataJpaCreditCardApplicationTests {
 
+	final String CREDIT_CARD = "12345678900000";
+
+	@Autowired
+	CreditCardRepository creditCardRepo;
+
+
 	@Test
-	void contextLoads() {
+	void testSaveAndStoreCreditCard() {
+		CreditCard cc = new CreditCard();
+		cc.setCreditCardNumber(CREDIT_CARD);
+		cc.setCcv("123");
+		cc.setExpirationDate("12/2028");
+
+		CreditCard saved = creditCardRepo.saveAndFlush(cc);
+
+		System.out.println("Getting CC from database");
+
+		CreditCard fetched = creditCardRepo.findById(saved.getId()).get();
+
+		assertThat(saved.getCreditCardNumber()).isEqualTo(fetched.getCreditCardNumber());
 	}
 
 }
