b5dcf5149bbbd534d40d2268ef79374c5e0b8a09
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-31 15:23:07 -0400

    Added ListenerRegistration that uses the 3 listeners, this is messy

---
+++ src/main/java/com/blurdel/sdjpa/creditcard/config/ListenerRegistration.java
@@ -0,0 +1,51 @@
+package com.blurdel.sdjpa.creditcard.config;
+
+import com.blurdel.sdjpa.creditcard.listeners.PostLoadListener;
+import com.blurdel.sdjpa.creditcard.listeners.PreInsertListener;
+import com.blurdel.sdjpa.creditcard.listeners.PreUpdateListener;
+import org.hibernate.SessionFactory;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
+import org.hibernate.event.spi.PostLoadEventListener;
+import org.hibernate.internal.SessionFactoryImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ListenerRegistration implements BeanPostProcessor {
+
+    private final PostLoadListener postLoadListener;
+    private final PreInsertListener preInsertListener;
+    private final PreUpdateListener preUpdateListener;
+
+
+    public ListenerRegistration(PostLoadListener postLoadListener, PreInsertListener preInsertListener, PreUpdateListener preUpdateListener) {
+        this.postLoadListener = postLoadListener;
+        this.preInsertListener = preInsertListener;
+        this.preUpdateListener = preUpdateListener;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+
+        if (bean instanceof LocalContainerEntityManagerFactoryBean) {
+            LocalContainerEntityManagerFactoryBean lemf = (LocalContainerEntityManagerFactoryBean) bean;
+            SessionFactoryImpl sessionFactory = (SessionFactoryImpl) lemf.getNativeEntityManagerFactory();
+            EventListenerRegistry registry = sessionFactory.getServiceRegistry()
+                    .getService(EventListenerRegistry.class);
+
+            registry.appendListeners(EventType.POST_LOAD, postLoadListener);
+            registry.appendListeners(EventType.PRE_INSERT, preInsertListener);
+            registry.appendListeners(EventType.PRE_UPDATE, preUpdateListener);
+        }
+
+        return bean;
+    }
+}
