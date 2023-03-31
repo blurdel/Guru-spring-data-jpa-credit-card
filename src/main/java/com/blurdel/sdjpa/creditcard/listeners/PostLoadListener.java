72c4e4d8043c9cc7fd30fec20ab0c47d114bac0e
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-31 15:34:52 -0400

    Added a AbstractEncryptionListener that the 3 listeners use to bring it all home ...

--- src/main/java/com/blurdel/sdjpa/creditcard/listeners/PostLoadListener.java
+++ src/main/java/com/blurdel/sdjpa/creditcard/listeners/PostLoadListener.java
@@ -1,15 +1,23 @@
 package com.blurdel.sdjpa.creditcard.listeners;
 
 
+import com.blurdel.sdjpa.creditcard.services.EncryptionService;
 import org.hibernate.event.spi.PostLoadEvent;
 import org.hibernate.event.spi.PostLoadEventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PostLoadListener implements PostLoadEventListener {
+public class PostLoadListener extends AbstractEncryptionListener implements PostLoadEventListener {
+
+
+    public PostLoadListener(EncryptionService encryptionService) {
+        super(encryptionService);
+    }
 
     @Override
     public void onPostLoad(PostLoadEvent event) {
         System.out.println("In Post-Load");
+
+        this.decrypt(event.getEntity());
     }
 }
--- src/main/java/com/blurdel/sdjpa/creditcard/listeners/PreUpdateListener.java
+++ src/main/java/com/blurdel/sdjpa/creditcard/listeners/PreUpdateListener.java
@@ -1,15 +1,22 @@
 package com.blurdel.sdjpa.creditcard.listeners;
 
+import com.blurdel.sdjpa.creditcard.services.EncryptionService;
 import org.hibernate.event.spi.PreUpdateEvent;
 import org.hibernate.event.spi.PreUpdateEventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PreUpdateListener implements PreUpdateEventListener {
+public class PreUpdateListener extends AbstractEncryptionListener implements PreUpdateEventListener {
+
+    public PreUpdateListener(EncryptionService encryptionService) {
+        super(encryptionService);
+    }
 
     @Override
     public boolean onPreUpdate(PreUpdateEvent event) {
         System.out.println("In Pre-Update");
+
+        this.encrypt(event.getState(), event.getPersister().getPropertyNames(), event.getEntity());
 
         return false;
     }
