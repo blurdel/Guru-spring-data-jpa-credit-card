a31a6e974d4fed965f20f4f8812fffb06996daef
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-31 15:52:58 -0400

    Callbacks - added CreditCardJPACallback that gets used in CreditCard annotated with EntityListeners

---
+++ src/main/java/com/blurdel/sdjpa/creditcard/domain/CreditCardJPACallback.java
@@ -0,0 +1,24 @@
+package com.blurdel.sdjpa.creditcard.domain;
+
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.PostUpdate;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+public class CreditCardJPACallback {
+
+    @PrePersist
+    @PreUpdate
+    public void beforeInsertOrUpdate(CreditCard creditCard) {
+        System.out.println("before insert/update was called ...");
+    }
+
+    @PostPersist
+    @PostLoad
+    @PostUpdate
+    public void postLoad(CreditCard creditCard) {
+        System.out.println("Post Load was called ...");
+    }
+
+}
