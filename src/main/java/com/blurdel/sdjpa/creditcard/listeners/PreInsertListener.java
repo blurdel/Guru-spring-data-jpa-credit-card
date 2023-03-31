20a6e1cccbd940caba8d19187d95afa01a504a2b
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-31 13:36:24 -0400

    Created 3 stub listeners

---
+++ src/main/java/com/blurdel/sdjpa/creditcard/listeners/PreInsertListener.java
@@ -0,0 +1,16 @@
+package com.blurdel.sdjpa.creditcard.listeners;
+
+import org.hibernate.event.spi.PreInsertEvent;
+import org.hibernate.event.spi.PreInsertEventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PreInsertListener implements PreInsertEventListener {
+
+    @Override
+    public boolean onPreInsert(PreInsertEvent event) {
+        System.out.println("In Pre-Insert");
+
+        return false;
+    }
+}
---
+++ src/main/java/com/blurdel/sdjpa/creditcard/listeners/PreUpdateListener.java
@@ -0,0 +1,16 @@
+package com.blurdel.sdjpa.creditcard.listeners;
+
+import org.hibernate.event.spi.PreUpdateEvent;
+import org.hibernate.event.spi.PreUpdateEventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PreUpdateListener implements PreUpdateEventListener {
+
+    @Override
+    public boolean onPreUpdate(PreUpdateEvent event) {
+        System.out.println("In Pre-Update");
+
+        return false;
+    }
+}
